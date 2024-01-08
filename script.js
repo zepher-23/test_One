e87f12a06ba7834ca5ea6a932f0278b8fc3555bf
zepher-23 <30zepher@gmail.com>
2024-01-08 20:34:32 +0530

    new update

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function test () {
-    var x = 1;
-}
+let array = [1,2,3,4,5]
+
+// for(let i=0;i<array.length;i++){
+//     console.log(array[i])
+// }
+
+// let newArray = array.map(function (item){
+//     return item * 2
+// })
+
+// array.forEach(function(item){
+//     console.log(item*2)
+// })
+
+
+let filtered = array.filter(function (item){return item == 2})
+console.log(filtered);
+
+// array.push(6)
+
+// let popped = array.pop()
+// console.log(popped)
+
+
+
+
+
+
