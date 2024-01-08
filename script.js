a8de82f695be5143a67b9c27ea7bace41015b8e6
zepher-23 <30zepher@gmail.com>
2024-01-08 21:11:37 +0530

    new update

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-let array = [1,2,3,4,5]
+// let array = [1,2,3,4,5]
 
-// for(let i=0;i<array.length;i++){
-//     console.log(array[i])
-// }
+// // for(let i=0;i<array.length;i++){
+// //     console.log(array[i])
+// // }
 
 // let newArray = array.map(function (item){
 //     return item * 2
 // })
 
-// array.forEach(function(item){
-//     console.log(item*2)
-// })
+// // array.forEach(function(item){
+// //     console.log(item*2)
+// // })
 
 
-let filtered = array.filter(function (item){return item == 2})
-console.log(filtered);
+// // let filtered = array.filter(function (item){return item == 2})
+// // console.log(filtered);
 
-// array.push(6)
+// // array.push(6)
 
-// let popped = array.pop()
-// console.log(popped)
+// let sum = array.reduce(function (accumulator, initialValue){
+//     return accumulator + initialValue
+// },2);
 
 
+
+// var accumulator = 2
+// for(i=0;i<array.length;i++){
+//     accumulator = accumulator + array[i];
+// }
+
+// console.log(sum)
+// // let popped = array.pop()
+// // console.log(popped)
+
+
+
+
+
+
+//Objects -----------------
+
+//primitive data types
+//number, string, boolean, null, undefined
+
+//spcl primitive
+//symbol - used to identify object values
+
+//non
+
+//object: {} empty object
+//array
+//functions
+//dates
+//regEx
+
+
+
+let object = {
+    name : "John",
+    age : 35,
+    isMarried : false,
+    children : ["Mike","Sara"],
+    job :{ 
+        title :"Software Engineer",
+        company : "Google"
+    }
+}
+
+
+
+let bag = {
+    space:'5 liters',
+    items:[ 'laptop','phone','charger'],
+    addItem: function(item){
+        this.items.push(item)
+    }
+}
+
+
+bag.addItem('Power Bank')
+console.log(bag);
 
 
 
